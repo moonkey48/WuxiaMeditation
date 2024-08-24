fe187b4596c728a66d9972beea00746ac72d82af
moonkey48 <moonkey48@icloud.com>
2024-08-25 08:57:11 +0900

    refactor: OnboardingView 코드 가독성 개선

--- WuxiaMeditation/View/OnboardingView.swift
+++ WuxiaMeditation/View/OnboardingView.swift
@@ -10,71 +10,19 @@ import SwiftUI
 
 struct OnboardingView: View {
     @AppStorage("isOnboarding") private var isOnboarding: Bool = true
-    @State private var isShowChangeNotificationDate = false
-    @State private var dateList: [Date] = []
-    @State private var isStart = false
     @AppStorage("firstTimeString") var firstTimeString: String = "07:30"
     @AppStorage("secondTimeString") var secondTimeString: String = "18:00"
     @AppStorage("thirdTimeString") var thirdTimeString: String = "23:00"
+    @State private var isShowChangeNotificationDate = false
+    @State private var dateList: [Date] = []
+    @State private var isStart = false
     
     var body: some View {
         VStack(spacing: 0) {
             if isStart {
-                Spacer()
-                HStack {
-                    Spacer()
-                    Text("\(dummyMeditationSentenceList.randomElement()?.sentence ?? dummyMeditationSentenceList[0].sentence)")
-                        .font(.customTitle3)
-                        .foregroundStyle(.white)
-                        .multilineTextAlignment(.center)
-                        .lineSpacing(6.0)
-                    Spacer()
-                }
-                .padding(.bottom, 40)
-                
-                Text("入門中")
-                    .font(.customTitle3)
-                    .foregroundStyle(.white)
-                    .padding(.bottom, 16)
-                ProgressView()
-                    .tint(.white)
-                Spacer()
+                progressView
             } else {
-                Spacer()
-                Text("運氣調息")
-                    .foregroundStyle(.white)
-                    .font(.system(size: 34))
-                    .padding(.bottom, 16)
-                Text("일과 삶의 마음 지킴 명상앱\n운기조식運氣調息")
-                    .foregroundStyle(.white)
-                    .multilineTextAlignment(.center)
-                    .font(.customBody)
-                    .lineSpacing(6)
-                Spacer()
-                VStack(spacing: 10) {
-                    Text("아침, 저녁, 밤에 운기조식 알람을 보내드립니다.")
-                    Button {
-                        isShowChangeNotificationDate.toggle()
-                    } label: {
-                        Text("일정 변경")
-                            .opacity(0.7)
-                    }
-                }
-                .font(.customCaption)
-                .foregroundStyle(.primaryGreen)
-                .padding(.bottom, 30)
-                
-                LargeButtonView(title: "시작") {
-                    NotificationManager().sendNotification(dateList: dateList)
-                    withAnimation {
-                        isStart = true
-                    }
-                    DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
-                        withAnimation {
-                            isOnboarding = false
-                        }
-                    }
-                }
+                introView
             }
         }
         .padding(16)
@@ -91,8 +39,72 @@ struct OnboardingView: View {
     }
 }
 
+private extension OnboardingView {
+    @ViewBuilder
+    var progressView: some View {
+        Spacer()
+        HStack {
+            Spacer()
+            Text("\(dummyMeditationSentenceList.randomElement()?.sentence ?? dummyMeditationSentenceList[0].sentence)")
+                .font(.customTitle3)
+                .foregroundStyle(.white)
+                .multilineTextAlignment(.center)
+                .lineSpacing(6.0)
+            Spacer()
+        }
+        .padding(.bottom, 40)
+        
+        Text("入門中")
+            .font(.customTitle3)
+            .foregroundStyle(.white)
+            .padding(.bottom, 16)
+        ProgressView()
+            .tint(.white)
+        Spacer()
+    }
+    
+    @ViewBuilder
+    var introView: some View {
+        Spacer()
+        Text("運氣調息")
+            .foregroundStyle(.white)
+            .font(.system(size: 34))
+            .padding(.bottom, 16)
+        Text("일과 삶의 마음 지킴 명상앱\n운기조식運氣調息")
+            .foregroundStyle(.white)
+            .multilineTextAlignment(.center)
+            .font(.customBody)
+            .lineSpacing(6)
+        Spacer()
+        VStack(spacing: 10) {
+            Text("아침, 저녁, 밤에 운기조식 알람을 보내드립니다.")
+            Button {
+                isShowChangeNotificationDate.toggle()
+            } label: {
+                Text("일정 변경")
+                    .opacity(0.7)
+            }
+        }
+        .font(.customCaption)
+        .foregroundStyle(.primaryGreen)
+        .padding(.bottom, 30)
+        
+        LargeButtonView(title: "시작") {
+            NotificationManager().sendNotification(dateList: dateList)
+            withAnimation {
+                isStart = true
+            }
+            DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+                withAnimation {
+                    isOnboarding = false
+                }
+            }
+        }
+    }
+}
+
 /// handle notifications
-extension OnboardingView {
+private extension OnboardingView {
     func setDateFromUserDefaults() {
         let dateFormmater = DateFormatter()
         dateFormmater.dateFormat = "HH:mm"
